c9cef00ef95bcee8bcc05b931169d97010098ba5
ArtyomKub <kub.killjoy@gmail.com>
2022-12-13 17:18:15 +0300

    Complete a universal button

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import './App.css';
+import {Button} from "./components/Button";
+
 
 function App() {
 
+    const Button1Foo = (subscriber: string, age: number) => {
+        console.log(subscriber, age)
+    }
+
+    const Button2Foo = (subscriber: string) => {
+        console.log(subscriber)
+    }
+
+    const Button3Foo = () => {
+        console.log('STUPID BUTTON')
+    }
+    
     return (
         <div>
-            <button>MyYouTubeChanel-1</button>
-            <button>MyYouTubeChanel-2</button>
-
+            <Button name={'MyYouTubeChanel-1'} callBack={() => Button1Foo('IGOR', 21)}/>
+            <Button name={'MyYouTubeChanel-2'} callBack={() => Button2Foo('IVAN')}/>
+            <Button name={'StupidButton'} callBack={Button3Foo}/>
         </div>
     );
 }
