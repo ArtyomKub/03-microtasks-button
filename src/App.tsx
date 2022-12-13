f63129f7c6e36b2fa6e0bc099ab3e866ce5e0073
ArtyomKub <kub.killjoy@gmail.com>
2022-12-13 12:54:30 +0300

    Make a universal button

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 
 function App() {
+
     return (
         <div>
             <button>MyYouTubeChanel-1</button>
             <button>MyYouTubeChanel-2</button>
+
         </div>
     );
 }
